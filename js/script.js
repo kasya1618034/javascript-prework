75947f0c1754041511e0ed5b6358c0f4bcebacb4
Katarzyna Filas <katarzyna.filas@gmail.com>
2021-06-22 15:40:08 +0200

    Add corrected information in js/script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 let playerMove = `papier`;
 
-printMessage('Zagrałeś ' + playerMove + '! Mój ruch to papier, więc mamy remis!');
+printMessage('Zagrałeś ' + playerMove + '! Mój ruch to kamień, więc wygrywasz!');
